5e00f1df4127cc197d96bf4d55764c6ca9b4472b
ly-mac <ghty010@163.com>
2022-10-06 11:15:15 +0800

    sagittal boundary label

--- code/utils/nii_utils.py
+++ code/utils/nii_utils.py
@@ -7,7 +7,8 @@ def read_patient_nii_mask(UID, seg_dir):
     seg_mask = nib.load(nii_path).get_fdata()
     seg_mask = np.rot90(seg_mask, axes=(0, 1))
     seg_mask = seg_mask.transpose((2, 0, 1))
-    seg_mask = np.clip(seg_mask, a_min=0, a_max=7)
+    seg_mask = np.flip(seg_mask, axis=0)
+    seg_mask[seg_mask > 7] = 0
     return seg_mask
 
 def get_uid_from_niipath(path):
